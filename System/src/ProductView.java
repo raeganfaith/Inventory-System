41257210d20f37987c112501377a36201cf9c5eb
raeganfaith <57030395+raeganfaith@users.noreply.github.com>
2021-06-13 10:49:02 +0800

    product inventory

--- System/src/ProductView.java
+++ System/src/ProductView.java
@@ -9,12 +9,21 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.SystemColor;
+import javax.swing.JTextField;
+import javax.swing.JButton;
 
 public class ProductView extends JFrame {
 	
 	private Image img_logo = new ImageIcon(ProductView.class.getResource("ress/logoh.png")).getImage().getScaledInstance(70, 100, Image.SCALE_SMOOTH);
 	
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
 
 	/**
 	 * Launch the application.
@@ -71,6 +80,84 @@ public class ProductView extends JFrame {
 		panel.setBackground(new Color(220, 220, 220));
 		panel.setBounds(0, 132, 700, 10);
 		contentPane.add(panel);
+		
+		JLabel lblNewLabel_2 = new JLabel("PRODUCT INVENTORY");
+		lblNewLabel_2.setForeground(SystemColor.menu);
+		lblNewLabel_2.setFont(new Font("Big John", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(242, 152, 251, 24);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Product ID:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3.setBounds(10, 192, 87, 24);
+		contentPane.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_3_1 = new JLabel("Name:");
+		lblNewLabel_3_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3_1.setBounds(10, 226, 100, 24);
+		contentPane.add(lblNewLabel_3_1);
+		
+		textField = new JTextField();
+		textField.setBounds(102, 197, 139, 19);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JLabel lblNewLabel_3_1_1 = new JLabel("Category:");
+		lblNewLabel_3_1_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3_1_1.setBounds(10, 259, 87, 24);
+		contentPane.add(lblNewLabel_3_1_1);
+		
+		JLabel lblNewLabel_3_1_2 = new JLabel("Quantity:");
+		lblNewLabel_3_1_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3_1_2.setBounds(10, 293, 87, 24);
+		contentPane.add(lblNewLabel_3_1_2);
+		
+		JLabel lblNewLabel_3_1_3 = new JLabel("Price:");
+		lblNewLabel_3_1_3.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3_1_3.setBounds(10, 327, 87, 24);
+		contentPane.add(lblNewLabel_3_1_3);
+		
+		JLabel lblNewLabel_3_1_4 = new JLabel("Description:");
+		lblNewLabel_3_1_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		lblNewLabel_3_1_4.setBounds(10, 361, 87, 24);
+		contentPane.add(lblNewLabel_3_1_4);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(102, 231, 139, 19);
+		contentPane.add(textField_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		textField_2.setBounds(102, 264, 139, 19);
+		contentPane.add(textField_2);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setBounds(102, 298, 139, 19);
+		contentPane.add(textField_3);
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		textField_4.setBounds(102, 332, 139, 19);
+		contentPane.add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setColumns(10);
+		textField_5.setBounds(102, 366, 139, 58);
+		contentPane.add(textField_5);
+		
+		JButton btnNewButton = new JButton("ADD");
+		btnNewButton.setBounds(10, 441, 100, 30);
+		contentPane.add(btnNewButton);
+		
+		JButton btnEdit = new JButton("EDIT");
+		btnEdit.setBounds(141, 441, 100, 30);
+		contentPane.add(btnEdit);
+		
+		JButton btnDelete = new JButton("DELETE");
+		btnDelete.setBounds(74, 481, 100, 30);
+		contentPane.add(btnDelete);
 	}
 
 }
